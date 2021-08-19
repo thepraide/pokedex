800413b7a987e300ceaf896a7a13653c723f4f29
Ricardo H <ricardo.hernandez@walmart.com>
2021-08-19 00:47:36 -0500

    Add pokedex Xcode project

---
+++ Pokedex App/Pokedex/CameraController.swift
@@ -0,0 +1,75 @@
+//
+//  CameraController.swift
+//  Pokedex
+//
+//  Created by Ricardo Hernandez on 18/08/21.
+//
+
+import UIKit
+import AVFoundation
+
+final class CameraController: UIViewController {
+    
+    private lazy var session: AVCaptureSession? = {
+        let session = AVCaptureSession()
+        let dataOutput = AVCaptureVideoDataOutput()
+        guard let captureDevice = AVCaptureDevice.default(for: .video),
+              let videoInput = try? AVCaptureDeviceInput(device: captureDevice),
+              session.canAddInput(videoInput),
+              session.canAddOutput(dataOutput) else { return nil }
+        session.addInput(videoInput)
+        session.addOutput(dataOutput)
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        session.startRunning()
+        return session
+        
+    }()
+    
+    private lazy var previewCameraLayer: AVCaptureVideoPreviewLayer? = {
+        guard let session = session else {
+            return nil
+        }
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = cameraView.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        return previewLayer
+    }()
+    
+    @IBOutlet weak var cameraView: UIView!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let cameraLayer = previewCameraLayer else {
+            fatalError("Tu dispositivo no soporta lectura de QR. Usa un dispositivo con camara")
+        }
+        cameraView.layer.addSublayer(cameraLayer)
+    }
+    
+    @IBAction func pokeButtonClicked(_ sender: Any) {
+        playPokedexSound()
+        showPokemonClassification()
+    }
+    
+    private func playPokedexSound() {
+        if let soundURL = Bundle.main.url(forResource: "pokedex", withExtension: "wav") {
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            AudioServicesPlaySystemSound(mySound);
+        }
+    }
+    
+    private func showPokemonClassification() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let resultController = storyboard.instantiateViewController(withIdentifier: "ResultController") as? ResultController else { return }
+        resultController.modalPresentationStyle = .pageSheet
+        present(resultController, animated: true, completion: nil)
+    }
+}
+
+extension CameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+    }
+}
---
+++ Pokedex App/Pokedex/ResultController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultController.swift
+//  Pokedex
+//
+//  Created by Ricardo Hernandez on 18/08/21.
+//
+
+import UIKit
+
+final class ResultController: UIViewController {
+    
+    @IBOutlet weak private var nameLabel: UILabel!
+        
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+}
